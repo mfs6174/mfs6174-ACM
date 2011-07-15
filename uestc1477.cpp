9850b822fa85c8ce058443dc6505ce2c38154c23
Xijin Zhang <mfs6174@gmail.com>
2011-07-15 13:50:25 +0800

    CDOJ 1477 1A 单调队列优化，开两个单调队列，一个从前往后更新，一个从后往前更新，用i的单调队列更新i+1的值

--- uestc1477.cpp
+++ uestc1477.cpp
@@ -10,7 +10,7 @@ LANG: C++
 #include<sstream>
 #include<cstring>
 #include<algorithm>
-#include<dqueue>
+#include<deque>
 #include<vector>
 
 using namespace std;
@@ -23,8 +23,8 @@ struct D
   int p,y;
 };
 
-dqueue<D> qian,hou;
-int i,j,k,n,m,t,a,k,zz,zu,y;
+deque<D> qian,hou;
+int i,j,k,n,m,t,a,zz,zu,y,mm;
 int s[1010],f[1010][1010];
 D tt,ttt;
 
@@ -35,45 +35,50 @@ inline int fang(int x)
 
 int main()
 {
-  inf>>zu;
+  cin>>zu;
   for (zz=1;zz<=zu;zz++)
   {
-    inf>>n>>k>>a;
+    cin>>n>>k>>a;
     for (i=1;i<=n;i++)
-      inf>>s[i];
+      cin>>s[i];
     qian.clear();hou.clear();
     memset(f,-1,sizeof(f));
     t=max(1000,s[1]+k);
     for (i=min(0,s[1]-k);i<=t;i++)
     {
       f[1][i]=fang(i-s[1]);
-      tt.y=f[1][i]-a*i;
-      tt.p=i;
-      while ((!qian.empty())&&((qian.back()).y>=tt.y))
-        qian.pop_back();
-      qian.push_back(tt);
-      tt.y=f[1][i]+a*i;
-      while ((!hou.empty())&&((hou.back()).y>=tt.y))
-        hou.pop_back();
-      hou.push_back(tt);
+     
     }
-    for (i=2;i<=n;i++)
+    for (i=1;i<n;i++)
     {
+      qian.clear();hou.clear();
       for (j=0;j<=1000;j++)
       {
-        while (qian.front().p<j-k) qian.pop_front();
-        f[i][j]=qian.front().y+a*j;
-        while (hou.back().p>j+k) hou.pop_back();
-        f[i][j]=min(f[i][j],hou.back().y-a*j);
-        f[i][j]+=fang(s[i]-j);
         tt.y=f[i][j]-a*j;
         tt.p=j;
         while ((!qian.empty())&&((qian.back()).y>=tt.y))
           qian.pop_back();
         qian.push_back(tt);
+        while (qian.front().p<j-k) qian.pop_front();
+        f[i+1][j]=qian.front().y+a*j;
+      }
+      for (j=1000;j>=0;j--)
+      {
         tt.y=f[i][j]+a*j;
+        tt.p=j;
         while ((!hou.empty())&&((hou.back()).y>=tt.y))
           hou.pop_back();
         hou.push_back(tt);
+         while (hou.front().p>j+k) hou.pop_front();
+         f[i+1][j]=min(f[i+1][j],hou.front().y-a*j);
+         f[i+1][j]+=fang(s[i+1]-j);
+      }
     }
-    
+    mm=maxlongint;
+    for (i=0;i<=1000;i++)
+      mm=min(mm,f[n][i]);
+    cout<<mm<<endl;
+  }
+  return 0;
+}
+
