5619491809ab07401850dc24a9971438e7cbfb86
Xijin Zhang <mfs6174@gmail.com>
2011-07-16 20:13:17 +0800

    CD 1504 简化代码，把DP过程写成函数，注意，所谓上下界如果不好处理就只处理上界，然后作差（多次DP）

--- uestc1504.cpp
+++ uestc1504.cpp
@@ -18,9 +18,9 @@ ifstream inf("ti.in");
 //ofstream ouf("ti.out");
 const int maxlongint=2147483647;
 
-long long i,j,k,n,m,t,p,zz,zu,a,b,d,o;
+long long i,j,k,n,m,t,p,zz,zu,a,mm1,mm2;
 long long f[30][12][2][2],ff[30][12],g[30][12],shu[30];
-string s,ss,xian;
+string s,ss;
 
 string str(long long x)
 {
@@ -29,6 +29,58 @@ string str(long long x)
     return t.str();
 }
 
+long long mfs(string &s)
+{
+  long long m=s.size(),mm=0,i,j,o,d;
+  memset(f,0,sizeof(f));
+  for (i=1;i<s[0]-48;i++)
+    f[1][i][0][1]=1;
+  f[1][s[0]-48][0][0]=1;
+  for (i=2;i<=m;i++)
+    for (j=0;j<=9;j++)
+    {
+      int t;
+      if (i>=3)
+        t=1;
+      else
+        t=0;
+      for (d=0;d<=t;d++)
+      {
+        if (!d)
+        {
+          if (j<s[i-1]-48)
+          {
+            for (o=0;o<j;o++)
+              f[i][j][d][1]+=f[i-1][o][d][0]+f[i-1][o][d][1];
+          }
+          else
+            for (o=0;o<j;o++)
+              f[i][j][d][1]+=f[i-1][o][d][1];
+          if (j==s[i-1]-48)
+            if (s[i-1]>s[i-2])
+              f[i][j][d][0]+=f[i-1][s[i-2]-48][d][0];
+          
+        }
+        else
+        {
+          if (j<s[i-1]-48)
+              for (o=j+1;o<=9;o++)
+                f[i][j][d][1]+=f[i-1][o][1][0]+f[i-1][o][0][0]+f[i-1][o][d][1]+f[i-1][o][0][1];
+          else
+            for (o=j+1;o<=9;o++)
+              f[i][j][d][1]+=(f[i-1][o][d][1]+f[i-1][o][0][1]);
+          if (j==s[i-1]-48)
+            if (s[i-1]<s[i-2])
+              f[i][j][d][0]+=(f[i-1][s[i-2]-48][d][0]+f[i-1][s[i-2]-48][0][0]);              
+        }
+      }
+    }
+  for (i=0;i<=9;i++)
+    mm+=f[m][i][1][0]+f[m][i][1][1];
+  return mm;
+}
+  
+  
 int main()
 {
   for (i=1;i<=9;i++)
@@ -54,112 +106,11 @@ int main()
   {
     cin>>a>>ss;
     s=str(a-1);
-    m=s.size();
-    memset(f,0,sizeof(f));
-    for (i=1;i<s[0]-48;i++)
-      f[1][i][0][1]=1;
-    f[1][s[0]-48][0][0]=1;
-    for (i=2;i<=m;i++)
-      for (j=0;j<=9;j++)
-      {
-        int t;
-        if (i>=3)
-          t=1;
-        else
-          t=0;
-        for (d=0;d<=t;d++)
-        {
-          if (!d)
-          {
-            if (j<s[i-1]-48)
-            {
-              // if (j==0)
-              //   f[i][j][d][1]=1;
-              for (o=0;o<j;o++)
-                f[i][j][d][1]+=f[i-1][o][d][0]+f[i-1][o][d][1];
-            }
-            else
-              for (o=0;o<j;o++)
-                f[i][j][d][1]+=f[i-1][o][d][1];
-            if (j==s[i-1]-48)
-              if (s[i-1]>s[i-2])
-                f[i][j][d][0]+=f[i-1][s[i-2]-48][d][0];
-             
-          }
-          else
-          {
-            if (j<s[i-1]-48)
-              for (o=j+1;o<=9;o++)
-              f[i][j][d][1]+=f[i-1][o][1][0]+f[i-1][o][0][0]+f[i-1][o][d][1]+f[i-1][o][0][1];
-            else
-              for (o=j+1;o<=9;o++)
-                f[i][j][d][1]+=(f[i-1][o][d][1]+f[i-1][o][0][1]);
-            if (j==s[i-1]-48)
-              if (s[i-1]<s[i-2])
-                f[i][j][d][0]+=(f[i-1][s[i-2]-48][d][0]+f[i-1][s[i-2]-48][0][0]);              
-          }
-        }
-      }
-    long long  mm1=0;
-      for (i=0;i<=9;i++)
-        mm1+=f[m][i][1][0]+f[m][i][1][1];
-    memset(f,0,sizeof(f));
-    m=ss.size();
-    xian=s;
-    s=ss;
-    for (i=1;i<s[0]-48;i++)
-      f[1][i][0][1]=1;
-    f[1][s[0]-48][0][0]=1;
-    for (i=2;i<=m;i++)
-      for (j=0;j<=9;j++)
-      {
-        int t;
-        if (i>=3)
-          t=1;
-        else
-          t=0;
-        for (d=0;d<=t;d++)
-        {
-          if (!d)
-          {
-            if (j<s[i-1]-48)
-            {
-              // if (j==0)
-              //   f[i][j][d][1]=1;
-              for (o=0;o<j;o++)
-                f[i][j][d][1]+=f[i-1][o][d][0]+f[i-1][o][d][1];
-            }
-            else
-              for (o=0;o<j;o++)
-                f[i][j][d][1]+=f[i-1][o][d][1];
-            if (j==s[i-1]-48)
-              if (s[i-1]>s[i-2])
-                f[i][j][d][0]+=f[i-1][s[i-2]-48][d][0];
-              // else
-              //   f[i][j][d][0]=0;
-          }
-          else
-          {
-            if (j<s[i-1]-48)
-              for (o=j+1;o<=9;o++)
-              f[i][j][d][1]+=f[i-1][o][1][0]+f[i-1][o][0][0]+f[i-1][o][d][1]+f[i-1][o][0][1];
-            else
-              for (o=j+1;o<=9;o++)
-                f[i][j][d][1]+=f[i-1][o][d][1]+f[i-1][o][0][1];
-            if (j==s[i-1]-48)
-              if (s[i-1]<s[i-2])
-                f[i][j][d][0]+=(f[i-1][s[i-2]-48][d][0]+f[i-1][s[i-2]-48][0][0]);
-              // else
-              //   f[i][j][d][0]=0;
-          }
-        }
-      }
-    long long  mm2=0;
-    for (i=0;i<=9;i++)
-      mm2+=f[m][i][1][0]+f[m][i][1][1];
-    for (i=1;i<xian.size();i++)
-      mm1+=shu[i];
+    mm1=mfs(s);
+    mm2=mfs(ss);
     for (i=1;i<s.size();i++)
+      mm1+=shu[i];
+    for (i=1;i<ss.size();i++)
       mm2+=shu[i];
      cout<<"Case #"<<zz<<": "<<mm2-mm1<<endl;
   }
