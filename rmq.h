ee1fdfbf34646e2ea4d06d4bbdbe019ae8298f70
Xijin Zhang <mfs6174@gmail.com>
2011-07-22 17:20:57 +0800

    RMQ ST模板修正，正确加括号处理位移的运算顺序，另外把数组两维的顺序改对了

--- rmq.h
+++ rmq.h
@@ -2,25 +2,21 @@
 using namespace std;
 
 const int maxn=200000;
-int t,i,n,m,l,r,f[32][maxn];
+long long j,t,i,n,m,l,r,f[maxn][32];//f[i][j]为从i开始往后2^j个数中的最大值，f[i][0]要处理成a[i]
+//以下都是max的，所以f[i][j]初始值为0
 
-int max(int x,int y)
-{
-	return (x>y)?(x):y;
-}
 
-void rmqchuli(int x)
+void rmqchuli(long long x)//预处理，x为数字个数
 {
-	int i,j,t=(int)(log(x)/log(2));
+  long long i,j,t=(long long)(log((double)x)/log(2.0));
 	for (j=1;j<=t;j++)
-		for (i=n-1<<j+1;i>=i;i--)
-			f[i][j]=max(f[i][j-1],f[i+1<<(j-1)][j-1]);
+      for (i=x-(1<<j)+1;i>=1;i--)
+        f[i][j]=max(f[i][j-1],f[i+(1<<(j-1))][j-1]);
 }
 
-int cha(int x,int y)
+long long cha(long long x,long long y)//查询过程，x到y闭区间
 {
-	int k;
-	k=(int)(log(y-x+1)/log(2));
-	return max(f[x][k],f[y-1<<k+1][k]);
+	long long k;
+	k=(long long)(log((double)(y-x+1))/log(2.0));
+	return max(f[x][k],f[y-(1<<k)+1][k]);
 }
-
