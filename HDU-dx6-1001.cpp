76de5ddbece6120a259d50967f07e2b1bc3e4ac7
Xijin Zhang <mfs6174@gmail.com>
2011-07-30 16:06:33 +0800

    终于过了，精度问题的处理太神奇了，要＊变成整数，可是那个加减0.1为什么？不明白另外加了注释

--- HDU-dx6-1001.cpp
+++ HDU-dx6-1001.cpp
@@ -21,7 +21,7 @@ const int maxlongint=2147483647;
 #define MAXN 70000
 #define MAXT 500000
 
-double shu[MAXT];
+int shu[MAXT];
 int f[MAXT];
 int n,cc;
 
@@ -37,7 +37,7 @@ void bitinit()
   memset(shu,0,sizeof(shu));
 }
 
-void upc(int x,double d)
+void upc(int x,int d)
 {
   // if (x==0)
   //   return;
@@ -59,16 +59,6 @@ int downs(int f[],int x)
   }
   return s;
 }
-double downs(double f[],int x)
-{
-  double s=0;
-  while (x>0)
-  {
-    s+=f[x];
-    x-=lowbit(x);
-  }
-  return s;
-}
 
 struct C
 {
@@ -77,7 +67,7 @@ struct C
 struct D
 {
   int x,y;
-  double d;
+  int d;
 };
 
 int i,j,k,t,m,p;
@@ -85,7 +75,7 @@ D xing[MAXN];
 C cha[420000];
 int chap[420000],xingp[MAXN];
 int ge[210000];
-double deng[210000];
+int deng[210000];
 map<int,int> hash;
 
 inline bool cmpx(const int &a,const int &b)
@@ -107,7 +97,10 @@ int main()
     hash.clear();
     for (i=1;i<=n;i++)
     {
-      scanf("%d%d%lf",&xing[i].x,&xing[i].y,&xing[i].d);
+      double td;
+      scanf("%d%d%lf",&xing[i].x,&xing[i].y,&td);
+      td*=100;
+      xing[i].d=int(td < 0 ? td - 0.1 : td + 0.1);
       xingp[i]=i;
       hash[xing[i].x]=1;
     }
@@ -115,7 +108,7 @@ int main()
     {
       scanf("%d%d%d%d",&cha[i].x,&cha[i].y,&cha[i+m].x,&cha[i+m].y);
       chap[i]=i;chap[i+m]=i+m;
-      cha[i].x--;cha[i].y--;
+      cha[i].x--;cha[i].y--;//闭区域的处理方法：对左下角的点xy都－1，是对坐标减1再离散不是对离散后的编号减1！！
       hash[cha[i].x]=1;hash[cha[i+m].x]=1;
     }
     i=0;
@@ -123,15 +116,15 @@ int main()
     {
       i++;
       (*it).second=i;
-    }
+    }//用map对坐标做映射（并且自动排序），这样离散化最简单了。另外要对问题中所有坐标一起离散化比较方便，而不是只离散东西，不离散查询
     cc=i;
-    sort(&xingp[1],&xingp[n+1],cmpx);
+    sort(&xingp[1],&xingp[n+1],cmpx);//同样是离散化的问题，不用对整个点的结构体排序，只要重写比较函数对编号索引数组进行排序
     sort(&chap[1],&chap[2*m+1],cmpc);
     bitinit();
     p=1;
     for (i=1;i<=n;i++)
     {
-      while (xing[xingp[i]].y>cha[chap[p]].y&&p<=2*m)
+      while (xing[xingp[i]].y>cha[chap[p]].y&&p<=2*m)//树状数组二维转一维的方法，按y分层处理，加入完一层查询一层，上面y（最右x-最左x）-下面y（最右x-最坐x），这样处理不用加
       {
         if (chap[p]<=m)
         {
@@ -163,7 +156,7 @@ int main()
     }
     for (i=1;i<=m;i++)
     {
-      printf("%.2lf",deng[i]);
+      printf("%.2lf",deng[i]/100.0);
       cout<<'/'<<ge[i]<<endl;
     }
   }
