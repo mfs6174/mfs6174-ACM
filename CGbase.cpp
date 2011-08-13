6e94e24061c7c4696c5952f4a8e28f8e793bea06
Xijin Zhang <mfs6174@gmail.com>
2011-08-13 13:27:53 +0800

    计算几何基础 现有结构，距离，叉积

---
+++ CGbase.cpp
@@ -0,0 +1,64 @@
+/*
+ID: mfs6174
+PROG: ti
+LANG: C++
+*/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<cstring>
+#include<algorithm>
+#include<map>
+#include<vector>
+#define sf scanf
+using namespace std;
+ifstream inf("ti.in");
+//ofstream ouf("ti.out");
+//freopen("ti.i","r",stdin);
+const int maxlongint=2147483647;
+
+const double INF=1e200;
+const double Ling=1e-7;
+
+struct P 
+{ 
+  double x; 
+  double y;
+  //constructor
+  P(double a=0, double b=0) 
+  { x=a; y=b;}  
+}; 
+struct LINESEG 
+{ 
+	POINT s; 
+	POINT e; 
+	LINESEG(POINT a, POINT b) { s=a; e=b;} 
+	LINESEG() { } 
+}; 
+
+// 直线的解析方程 a*x+b*y+c=0  为统一表示，约定 a >= 0 
+struct L           
+{ 
+   double a; 
+   double b; 
+   double c; 
+   L(double d1=1, double d2=-1, double d3=0) {a=d1; b=d2; c=d3;} 
+};
+
+inline double dst(P p1,P p2)                
+{ 
+	return( sqrt( (p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y) ) ); 
+}
+
+inline int cha(P a,P b,P c)
+{
+  double x1,x2,y1,y2;
+  x1=b.x-a.x;
+  y1=b.y-a.y;
+  x2=c.x-b.x;
+  y2=c.y-b.y;
+  return x1*y2-x2*y1;
+}
+
