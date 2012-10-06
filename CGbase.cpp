593c75f6f7b9e29e337d74dc88940059e2519dc1
Xijin Zhang <mfs6174@gmail.com>
2012-10-07 02:55:24 +0800

    添加多边形重心

--- CGbase.cpp
+++ CGbase.cpp
@@ -188,4 +188,27 @@ inline double polys(P shu[],int n)//多边形面积
   return ss;
 }
 
+P gravity(P *p, int n)//多边形重心
+{
+  double area = 0;
+  P center;
+  center.x = 0;
+  center.y = 0;
+
+  for (int i = 0; i < n-1; i++)
+  {
+    area += (p[i].x*p[i+1].y - p[i+1].x*p[i].y)/2;
+    center.x += (p[i].x*p[i+1].y - p[i+1].x*p[i].y) * (p[i].x + p[i+1].x);
+    center.y += (p[i].x*p[i+1].y - p[i+1].x*p[i].y) * (p[i].y + p[i+1].y);
+  }
+
+  area += (p[n-1].x*p[0].y - p[0].x*p[n-1].y)/2;
+  center.x += (p[n-1].x*p[0].y - p[0].x*p[n-1].y) * (p[n-1].x + p[0].x);
+  center.y += (p[n-1].x*p[0].y - p[0].x*p[n-1].y) * (p[n-1].y + p[0].y);
+
+  center.x /= 6*area;
+  center.y /= 6*area;
+
+  return center;
+}
 
